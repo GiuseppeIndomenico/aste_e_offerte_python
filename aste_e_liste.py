a73b1bef2a07a278b25240c98426665dad3f4b60
Giuseppe Indomenico <giuseppe.indomenico91@gmail.com>
2024-12-17 11:55:32 +0100

    fix

--- aste_e_liste.py
+++ aste_e_liste.py
@@ -1,20 +1,5 @@
 import re
 
-# Si vuole simulare l`andamento di un`asta on line che vuole
-# sperimentare una conduzione innovativa; ogni partecipante
-# farà una sola offerta ed il pezzo messo all`asta sarà aggiudicato
-# al partecipante la cui offerta più si avvicina alla media
-# calcolata su tutte le offerte; ogni offerta sarà registrata
-# come una coppia [email, offerta_in_euro].
-#
-# Dovrà essere memorizzata una lista di tutte le offerte pervenute.
-
-# Al termine:
-# - dalla lista dovrà essere eliminata l`offerta minima e massima
-# - dovrà essere costruita una seconda lista contenente
-#   solo le offerte che non discostano più del 10% rispetto alla media
-#   calcolata su tutte le offerte rimaste
-
 lista_offerte=[["a@a.com",10], ["b@a.com",15],["c@a.com",55], ["e@a.com",58], ["f@a.com",100], ["d@a.com",150]]
 altre_offerte=True
 
